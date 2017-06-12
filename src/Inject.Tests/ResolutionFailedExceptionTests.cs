be30b18afbe6d0b78c9f6fae5ec4b6a452ef4c79
Robert Coltheart <robert.coltheart@gmail.com>
2017-06-13 07:47:26 +0800

    [Feature] Migrate to netstandard1.0

--- src/Inject.Tests/ResolutionFailedExceptionTests.cs
+++ src/Inject.Tests/ResolutionFailedExceptionTests.cs
@@ -30,36 +30,5 @@ namespace Inject
 
             Assert.AreEqual("msg", exception.Message);
         }
-
-        [Test]
-        public void IsSerializableDecorated()
-        {
-            Assert.IsTrue(typeof(ResolutionFailedException).GetCustomAttributes(typeof(SerializableAttribute), true).Length == 1, "Type must be serializable.");
-        }
-
-        [Test]
-        public void IsSerializable()
-        {
-            var exception = new ResolutionFailedException();
-
-            try
-            {
-                using (var stream = new MemoryStream())
-                {
-                    var formatter = new BinaryFormatter();
-                    formatter.Serialize(stream, exception);
-
-                    stream.Position = 0;
-
-                    exception = (ResolutionFailedException)formatter.Deserialize(stream);
-                }
-            }
-            catch (Exception)
-            {
-                Assert.Fail("Type must be serializable.");
-            }
-
-            Assert.IsNotNull(exception, "Type could not be deserialized.");
-        }
     }
 }
